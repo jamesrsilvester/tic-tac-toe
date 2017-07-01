0ad71f6bd001ab0f12cb6d62f76e956ed0f481c1
jamesrsilvester <jamesrsilvester@gmail.com>
2017-07-01 13:30:36 -0700

    long way to go

--- js/app.js
+++ js/app.js
@@ -2,5 +2,39 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-
+  console.log("Js linked");
+  $(document).ready(function() {
+    console.log(" and ready!");
+  })
 });
+
+  //SELECT ALL with class .box
+let $boxes = $('.box');
+let $red = $('.red'); //RED goes first
+let $blue = $('.blue');
+
+//    ///create var numTurns, set = 2. (at end of add class function, numTurns+1)
+//
+//         ///add to
+//
+//
+// ////add event listener to class = .row
+//
+//
+// //ONCE CLICKED//
+//
+//
+//
+//     //// find target of click.
+
+      //// change target class based on odd or even
+      //  if changeClass = numTurns.length % 2 === 0 &&& target.class !== ? target.addclass "red": target.addclass "red" )
+
+      //// add +1 to numTurns.
+
+      /////
+
+//Win - X = 1 and O = 0, if ($red.length adds up to
+
+////CONCERNS
+// Locality of click.target. Save as global? Refresh on new click?
