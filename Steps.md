584d8f29f64c401ffb5bcb2d19f54e69bba81fa8
jamesrsilvester <jamesrsilvester@gmail.com>
2017-07-04 11:04:26 -0700

    turn tracking added

---
+++ Steps.md
@@ -0,0 +1,6 @@
+#TIC TAC TOE
+## 
+### Solar System Exploration, 1950s – 1960s
+- [ ] Mercury
+- [ ]
+- [x] Venus
--- js/app.js
+++ js/app.js
@@ -2,24 +2,41 @@
 $(document).ready(function() {
   // all code to manipulate the DOM goes inside this function
   console.log("Js linked");
-  //
+  //assigns overall listener as gameboard
   let $gameboard = $('#board');
   //prints box clicked to console.
   $gameboard.on("click", function (event) {
-   console.log(event.target.id);
+   console.log(`${event.target.id} was clicked`);
+   let idBoxClicked = event.target.id;
+     if (numTurns%2===0) {
+       console.log(`Player 1 clicked ${event.target.id}`);
+       }
+       else {console.log(`Player 2 clicked ${event.target.id}`);
+       }
+     changeClass();
+     addTurn();
   });
   //confirms that clicks are recorded?
   console.log(" and listening for clicks");
 });
 
 //Create variables here that do not require DOM load.
-
 let $boxes = $('.box'); //// Selects all boxes
 let $red = $('.red'); //RED goes first
 let $blue = $('.blue'); //BLUE goes second
 let $row = $('.row'); //ROW contains the entire board and is parent event listener
+let numTurns = 2; // Starts at Even, starts with 2 rather than 0 for %.
 
-//    ///create var numTurns, set = 2. (at end of add class function, numTurns+1)
+//Change Class -- INCOMPLETE//
+let changeClass = function () {
+  console.log(`change class did nothing`);
+}
+
+//AddTurn
+let addTurn = function () {numTurns ++;
+console.log(`This is turn number ${numTurns-2}`);}; //-2 because counter started at 2.
+
+//
 
 // //ONCE CLICKED
 
