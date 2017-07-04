d11c6cb5713c21d166c536922c10066ec14999c6
jamesrsilvester <jamesrsilvester@gmail.com>
2017-07-04 16:57:45 -0700

    win conditions added

--- js/app.js
+++ js/app.js
@@ -5,21 +5,34 @@ $(document).ready(function() {
   //assigns overall listener as gameboard
   let $gameboard = $('#board');
 
-  // RESET BUTTON
-  $('#button').on("click", function () {location.reload()});
+  //Tracks Turns Made, add to array
+  moveTracker = function () {
+    movesMade.push(`${idBoxClicked}`);
+    console.log(`Previous moves are ${movesMade}`);
+    checkForWin();
+    }
+
+  // Reset button
+  hitReset = function () {location.reload()};
+  $('#button').on("click", hitReset);
+
 
   //prints box clicked to console.
   $gameboard.on("click", function (event) {
-   console.log(`${event.target.id} was clicked`);
-   let idBoxClicked = event.target.id;
+  //removed let from idBoxClicked//
+  idBoxClicked = event.target.id;
 
-   //Changes class of clicked element
+   //??Changes class of clicked element//
    let changeClass = function () {
+     //if turn number is even, and doesn't contain, box or red class:
      if (numTurns%2 === 0) {
        $(`#${idBoxClicked}`).removeClass( "box" ).addClass( "red" );
+       $(`span`).text( "Two" ).css({'color':"blue"});
      }
+     //if turn number is odd, and doesn't contain box or blue class:
      else {
        $(`#${idBoxClicked}`).removeClass( "box" ).addClass( "blue" );
+       $(`span`).text( "One" ).css({'color':"red"});
      }
    }
 
@@ -35,14 +48,17 @@ $(document).ready(function() {
          }
        changeClass();
        addTurn();
+       moveTracker();
         }
-        else {
+        else if (movesMade.length <= 9){
           alert("You already went there..");
         }
-
+        else {
+          hitReset();
+        }
 
   });
-  //confirms that clicks are recorded?
+  //confirms that clicks are being recorded
   console.log(" ... and listening for clicks");
 });
 
@@ -50,13 +66,67 @@ $(document).ready(function() {
 let $boxes = $('.box'); //// Selects all boxes
 let $red = $('.red'); //RED goes first
 let $blue = $('.blue'); //BLUE goes second
-let $row = $('.row'); //ROW contains the entire board and is parent event listener
 let numTurns = 2; // Starts at Even, starts with 2 rather than 0 for %.
+movesMade = []; //collects all turns
 
-
-//AddTurn
-let addTurn = function () {numTurns ++;
+//Add each turn to overall turn count
+let addTurn = function() {numTurns ++;
 console.log(`This is turn number ${numTurns-2}`);}; //-2 because counter started at 2.
 
 
-//WIN - X = 1 and O = 0, if ($red.length adds up to
+checkForWin = function () {
+if (
+  //Row 1
+  $('#b0').hasClass("red") && $('#b1').hasClass("red") && $('#b2').hasClass("red") ||
+  //Row 2
+  $('#b3').hasClass("red") && $('#b4').hasClass("red") && $('#b5').hasClass("red") ||
+  //Row 3
+  $('#b6').hasClass("red") && $('#b7').hasClass("red") && $('#b8').hasClass("red") ||
+  //Column 1
+  $('#b0').hasClass("red") && $('#b3').hasClass("red") && $('#b6').hasClass("red") ||
+  //Column 2
+  $('#b1').hasClass("red") && $('#b4').hasClass("red") && $('#b7').hasClass("red") ||
+  //Column 3
+  $('#b2').hasClass("red") && $('#b5').hasClass("red") && $('#b8').hasClass("red") ||
+  //Diagonal Upper Left to Bottom Right
+  $('#b0').hasClass("red") && $('#b4').hasClass("red") && $('#b8').hasClass("red") ||
+  //Diagonal Upper Right to Lower Left
+  $('#b2').hasClass("red") && $('#b4').hasClass("red") && $('#b6').hasClass("red")
+)
+//Win Sequence RED
+{$(`h3`).text( "RED is the winner!" ).css({'color':"red"});
+}
+//BLUE win condition
+else if (
+  //Row 1
+  $('#b0').hasClass("blue") && $('#b1').hasClass("blue") && $('#b2').hasClass("blue") ||
+  //Row 2
+  $('#b3').hasClass("blue") && $('#b4').hasClass("blue") && $('#b5').hasClass("blue") ||
+  //Row 3
+  $('#b6').hasClass("blue") && $('#b7').hasClass("blue") && $('#b8').hasClass("blue") ||
+  //Column 1
+  $('#b0').hasClass("blue") && $('#b3').hasClass("blue") && $('#b6').hasClass("blue") ||
+  //Column 2
+  $('#b1').hasClass("blue") && $('#b4').hasClass("blue") && $('#b7').hasClass("blue") ||
+  //Column 3
+  $('#b2').hasClass("blue") && $('#b5').hasClass("blue") && $('#b8').hasClass("blue") ||
+  // //Diagonal Upper Left to Bottom Right
+  $('#b0').hasClass("blue") && $('#b4').hasClass("blue") && $('#b8').hasClass("blue") ||
+  //Diagonal Upper Right to Lower Left
+  $('#b2').hasClass("blue") && $('#b4').hasClass("blue") && $('#b6').hasClass("blue")
+)
+//Win Sequence BLUE
+{$(`h3`).text( "BLUE is the winner!" ).css({'color':"blue"});
+}
+
+//setTimeout(function() {alert('hello');},250);
+
+
+else if (movesMade.length === 9)
+//Draw Sequence
+{
+$(`h3`).text( "It's a DRAW" ).css({'color':"black"});}
+
+else if (movesMade.length === 10)
+{hitReset();}
+}
